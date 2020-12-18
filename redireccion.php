4a2d82e25d51e0d51549fa237cdb69cda9a69b19
JuanTrop <jortiza2@unicartagena.edu.co>
2020-12-17 22:26:29 -0500

    Clase conexionbd y clase redireccionador

--- redireccion.php
+++ redireccion.php
@@ -9,14 +9,25 @@ if(isset($_POST['url'])){
     $conn = ConexionBDD::conectar();
 
    //Verificar que no existe la urlç
-    $query = $conn->prepare("SELECT origin_url FROM shortened WHERE origin_url = '$url' ");
-    $query->execute();
+    $sql = "SELECT origin_url FROM shortened WHERE origin_url = '$url' ";
+    $result = $conn->query($sql);
 
-    if($query != null){
-        echo "Ya existe una url acortada a este sitio" . $url ;
+    if($result->columnCount() == 0){
+        echo "Ya existe una url acortada a este sitio" . $url . "\n";
     } else {
         echo "Creando redirección";
-        $query = $conn->prepare("INSERT INTO shortened ");
+        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+    // Output: 54esmdr0qf
+        $shortened = substr(str_shuffle($permitted_chars), 0, 3);
+        $sql = "INSERT INTO shortened (origin_url, shortened) VALUES ('$url', '$shortened') ";
+        echo $shortened . "\n";
+        $result = $conn->query($sql);
+
+        if(!$result){
+            echo "Ingreso exitoso";
+        } else {
+            echo "Ingreso fallido";
+        }
     }
 
 } else {
